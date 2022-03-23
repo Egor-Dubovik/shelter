5e0d0a8b9e98f3342fc2b0002f7756ba14267dff
Egor Dubovik <egorka54896@gmail.com>
2022-03-23 13:20:46 +0300

    feat: created function "showPopap" and fix bug with "lock"

--- js_modules/_menuSwitching.js
+++ js_modules/_menuSwitching.js
@@ -1,33 +1,32 @@
-const menuBody = document.querySelector('.menu__body');
-const iconBurger = document.querySelector('.menu__icon');
-const blackoutBlock = document.querySelector('.blackout-screan');
-const menuLinks = document.querySelectorAll('.menu__link');
+const menuBody = document.querySelector(".menu__body");
+const iconBurger = document.querySelector(".menu__icon");
+const blackoutBlock = document.querySelector(".blackout-screan");
 
 function closeMenu(arr) {
-	arr.forEach(el => el.classList.remove('active'));
+	arr.forEach(el => el.classList.remove("active"));
 }
 
 function openMenu(...arr) {
-	arr.forEach(el => el.classList.toggle('active'));
+	arr.forEach(el => el.classList.toggle("active"));
+	document.body.classList.toggle("lock");
 }
 
 export const switchMenu = () => {
 	iconBurger.addEventListener("click", () => {
 		openMenu.apply(this, [menuBody, iconBurger, blackoutBlock]);
-		document.body.classList.toggle('lock');
 	});
 
 	menuBody.addEventListener("click", (e) => {
-		if (e.target.classList.contains('menu__link')) {
+		if (e.target.classList.contains("menu__link")) {
 			closeMenu([menuBody, iconBurger, blackoutBlock]);
-			document.body.classList.remove('lock');
+			document.body.classList.remove("lock");
 		}
 	});
 
-	document.addEventListener('mousedown', (e) => {
-		if (!e.target.closest('.menu')) {
+	document.addEventListener("mousedown", (e) => {
+		if (!e.target.closest(".menu") && !e.target.closest(".info-popap__content")) {
 			closeMenu([menuBody, iconBurger, blackoutBlock]);
-			document.body.classList.remove('lock');
+			document.body.classList.remove("lock");
 		}
 	});
 }
--- pages/main/script.js
+++ pages/main/script.js
@@ -1,7 +1,10 @@
 import { switchMenu } from "../../js_modules/_menuSwitching.js";
 import { changeViewHeader } from "../../js_modules/_changeViewHeader.js";
+import { showPopap } from "../../js_modules/_showPopap.js";
+
 changeViewHeader();
 switchMenu();
+showPopap();
 
 
 
