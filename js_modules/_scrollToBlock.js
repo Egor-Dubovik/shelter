6b7dc6f28133c3d3b2142bd9ffd4c2f8fa6ffe47
Egor Dubovik <egorka54896@gmail.com>
2022-03-23 21:07:18 +0300

    feat: change file scrollToBlock, add function "scrollDecorationLink"

--- js_modules/_scrollToBlock.js
+++ js_modules/_scrollToBlock.js
@@ -1,20 +1,49 @@
 const anchors = document.querySelectorAll("a[href*='#']");
+const sections = document.querySelectorAll("section");
 
 export function scrollToblock() {
 	for (let anchor of anchors) {
 		anchor.addEventListener("click", function (e) {
 			e.preventDefault();
 			const blockID = anchor.getAttribute("href");
-			scrollTo(document.querySelector("" + blockID));
+			scrollFeature(document.querySelector("" + blockID));
 		})
 
 	}
 }
 
-function scrollTo(event) {
+function scrollFeature(event) {
 	window.scroll({
 		left: 0,
 		top: event.offsetTop - 80.13,
 		behavior: "smooth"
 	})
-};
+}
+
+export function scrollDecorationLink() {
+	window.onscroll = () => {
+		let currentSection = "";
+
+		sections.forEach(section => {
+			let sectionTop = "";
+
+			if (section.id === "contacts") {
+				sectionTop = section.offsetTop - 50;
+			} else {
+				sectionTop = section.offsetTop;
+			}
+
+			if (window.pageYOffset >= sectionTop - 80) {
+				currentSection = section.getAttribute("id");
+			}
+		});
+
+		anchors.forEach(anchor => {
+			anchor.classList.remove("active");
+			const sectionId = anchor.getAttribute("href").slice(1);
+			if (sectionId === currentSection) {
+				anchor.classList.add("active");
+			}
+		});
+	}
+}
--- pages/main/script.js
+++ pages/main/script.js
@@ -1,10 +1,11 @@
 import { switchMenu } from "../../js_modules/_menuSwitching.js";
 import { changeViewHeader } from "../../js_modules/_changeViewHeader.js";
 import { switchPopap } from "../../js_modules/_switchPopap.js";
-import { scrollToblock } from "../../js_modules/_scrollToBlock.js";
+import { scrollToblock, scrollDecorationLink } from "../../js_modules/_scrollToBlock.js";
 
 changeViewHeader();
 scrollToblock();
+scrollDecorationLink();
 switchMenu();
 switchPopap();
 
